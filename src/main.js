87804aace0fa4d2a7351fe94de470a38ceecd5f3
Aleksandar Stanisic <ciandre23@gmail.com>
2026-05-19 19:19:27 +0200

    add bg test

--- src/main.js
+++ src/main.js
@@ -4,3 +4,20 @@ import * as THREE from "three";
 console.log(THREE);
 
 console.log("Hello Vite!");
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#bg"),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
