f12a4090efa601a836a4aff678dbf8ff257f6d76
Aleksandar Stanisic <ciandre23@gmail.com>
2026-05-27 19:07:19 +0200

    small fix

--- src/main.js
+++ src/main.js
@@ -10,7 +10,6 @@ const parameters = {
  */
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
-console.log(canvas);
 
 // Scene
 const scene = new THREE.Scene();
@@ -22,7 +21,7 @@ const mesh1 = new THREE.Mesh(
   new THREE.TorusGeometry(0.5, 0.3, 16, 100),
   new THREE.MeshBasicMaterial({ color: "red" }),
 );
-mesh1.position.y = -1.7;
+mesh1.position.y = -2.7;
 scene.add(mesh1);
 const mesh2 = new THREE.Mesh(
   new THREE.TorusGeometry(0.5, 0.3, 16, 100),
@@ -33,7 +32,7 @@ const mesh3 = new THREE.Mesh(
   new THREE.TorusGeometry(0.5, 0.3, 16, 100),
   new THREE.MeshBasicMaterial({ color: "blue" }),
 );
-mesh3.position.y = 1.7;
+mesh3.position.y = -5.4;
 scene.add(mesh3);
 
 /**
@@ -81,6 +80,11 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
+let scrollY = window.scrollY;
+window.addEventListener("scroll", () => {
+  scrollY = window.scrollY;
+});
+
 /**
  * Animate
  */
@@ -88,12 +92,16 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+  camera.position.y = -scrollY * sizes.height * 0.25 * 0.0001;
 
   // Render
   renderer.render(scene, camera);
   mesh1.rotation.x = 0.1 * elapsedTime;
+  mesh1.rotation.y = 0.1 * elapsedTime;
   mesh2.rotation.x = 0.2 * elapsedTime;
+  mesh2.rotation.y = 0.2 * elapsedTime;
   mesh3.rotation.x = 0.3 * elapsedTime;
+  mesh3.rotation.y = 0.3 * elapsedTime;
 
   // Call tick again on the next frame
   window.requestAnimationFrame(tick);
