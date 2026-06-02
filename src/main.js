8e95788af362a107bb4314f21bdc4d68f77a3a03
Aleksandar Stanisic <ciandre23@gmail.com>
2026-06-02 14:45:14 +0200

    fixing scene

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000,
 );
-camera.position.set(0, 0, 10);
+camera.position.set(0, 5, 10);
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector(".webgl"),
@@ -20,17 +20,20 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.render(scene, camera);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const terrain = new THREE.PlaneGeometry(50, 50, 100, 100);
+const terrainMaterial = new THREE.MeshStandardMaterial({ color: "#ff8000" });
+const terrainMesh = new THREE.Mesh(terrain, terrainMaterial);
+terrainMesh.rotation.x = -Math.PI / 2;
+scene.add(terrainMesh);
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(5, 5, 5);
+scene.add(light);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+
   renderer.render(scene, camera);
 }
 animate();
