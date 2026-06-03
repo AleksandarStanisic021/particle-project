053455c549649ace839c9d01967bee1286f9fbc2
Aleksandar Stanisic <ciandre23@gmail.com>
2026-06-03 14:34:23 +0200

    add objects

--- src/main.js
+++ src/main.js
@@ -11,10 +11,46 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000,
 );
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(5, 5, 5);
+scene.add(light);
+
+const sphere1 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 32, 32),
+  new THREE.MeshStandardMaterial({ color: "#ff0000" }),
+);
+sphere1.position.set(-2, 0, 0);
+scene.add(sphere1);
+
+const sphere2 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 32, 32),
+  new THREE.MeshStandardMaterial({ color: "#ff0000" }),
+);
+sphere2.position.set(2, 0, 0);
+scene.add(sphere2);
+
+const sphere3 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 32, 32),
+  new THREE.MeshStandardMaterial({ color: "#ff0000" }),
+);
+sphere3.position.set(0, 0, 0);
+scene.add(sphere3);
+
 camera.position.set(0, 5, 10);
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector(".webgl"),
+  antialias: true,
 });
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.render(scene, camera);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+const animate = function () {
+  requestAnimationFrame(animate);
+
+  renderer.render(scene, camera);
+};
+
+animate();
