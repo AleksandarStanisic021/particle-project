9f7bb60bdb2ad08840919134c40df60c54a76ff2
Aleksandar Stanisic <ciandre23@gmail.com>
2026-06-09 16:16:22 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -31,7 +31,11 @@ light.position.set(5, 5, 5);
 scene.add(light);
 
 const ballGeometry = new THREE.SphereGeometry(0.5, 32, 32);
-const ballMaterial = new THREE.MeshStandardMaterial({ color: "#ff0000" });
+const ballMaterial = new THREE.MeshStandardMaterial({
+  color: "#ff0000",
+  metalness: 0.9,
+  roughness: 0.5,
+});
 const ballMesh = new THREE.Mesh(ballGeometry, ballMaterial);
 ballMesh.position.set(0, 0.5, 0);
 scene.add(ballMesh);
