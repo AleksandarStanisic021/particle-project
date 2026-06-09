78b74da1dad14e1573dbe9285f80433bff9be47c
Aleksandar Stanisic <ciandre23@gmail.com>
2026-06-09 15:53:38 +0200

    reset scene

---
+++ src/main5.js
@@ -0,0 +1,137 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
+const scene = new THREE.Scene();
+
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+const environmentMapTexture = cubeTextureLoader.load([
+  "/environmentMaps/0/px.png",
+  "/environmentMaps/0/nx.png",
+  "/environmentMaps/0/py.png",
+  "/environmentMaps/0/ny.png",
+  "/environmentMaps/0/pz.png",
+  "/environmentMaps/0/nz.png",
+]);
+
+let model = null;
+const gltfLoader = new GLTFLoader();
+gltfLoader.load("/models/FlightHelmet/glTF/FlightHelmet.gltf", (gltf) => {
+  model = gltf?.scene;
+  console.log(gltf.scene);
+  if (model !== null) {
+    model.scale.set(10, 10, 10);
+    model.position.set(0, -4, 0);
+    scene.add(model);
+  }
+});
+
+const ballGeometry = new THREE.SphereGeometry(1.4, 32, 32);
+const ballMaterial = new THREE.MeshStandardMaterial({
+  color: "#f4e5e5",
+  roughness: 0.1,
+  metalness: 0.3,
+  envMap: environmentMapTexture,
+});
+const ball = new THREE.Mesh(ballGeometry, ballMaterial);
+ball.position.set(-5, 0, 0);
+scene.add(ball);
+
+let ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
+
+scene.environment = environmentMapTexture;
+scene.background = environmentMapTexture;
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(5, 5, 5);
+scene.add(light);
+
+/*
+const sphere1 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 32, 32),
+  new THREE.MeshStandardMaterial({ color: "#ff0000" }),
+);
+sphere1.position.set(-2, 0, 0);
+scene.add(sphere1);
+
+const sphere2 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 32, 32),
+  new THREE.MeshStandardMaterial({ color: "#ff0000" }),
+);
+sphere2.position.set(2, 0, 0);
+scene.add(sphere2);
+
+const sphere3 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 32, 32),
+  new THREE.MeshStandardMaterial({ color: "#ff0000" }),
+);
+sphere3.position.set(0, 0, 0);
+scene.add(sphere3);
+*/
+/*
+const mouse = new THREE.Vector2();
+window.addEventListener("mousemove", (event) => {
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+});
+*/
+
+camera.position.set(0, 5, 10);
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector(".webgl"),
+  antialias: true,
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+const raycaster = new THREE.Raycaster();
+
+let clock = new THREE.Clock();
+const animate = function () {
+  controls.update();
+  /*
+  const rayOrigin = new THREE.Vector3(-3, 0, 0);
+  const rayDirection = new THREE.Vector3(1, 0, 0).normalize();
+  raycaster.set(rayOrigin, rayDirection);
+
+  const ojectsToTest = [sphere1, sphere2, sphere3];
+  /*
+
+
+  const intersects = raycaster.intersectObjects(ojectsToTest);
+
+  for (let object of ojectsToTest) {
+    object.material.color.set("#ff0000");
+  }
+  for (const intersect of intersects) {
+    intersect.object.material.color.set("#0000ff");
+  }*/
+  /* raycaster.setFromCamera(mouse, camera);
+  const intersects = raycaster.intersectObjects(ojectsToTest);
+  if (intersects.length > 0) {
+    intersects[0].object.material.color.set("#ff00e1");
+  } else {
+    for (let object of ojectsToTest) {
+      object.material.color.set("#ff0000");
+    }
+  }
+*/
+
+  let delta = clock.getElapsedTime();
+
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+};
+
+animate();
