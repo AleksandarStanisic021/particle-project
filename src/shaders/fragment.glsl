b1c468090081b4e91aa248b8dbcf6c3a3893ae44
Aleksandar Stanisic <ciandre23@gmail.com>
2026-06-11 19:59:53 +0200

    fix

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -5,6 +5,6 @@ uniform float time;
 void main()
 {
     float t = time;
-    gl_FragColor = vec4(0.5 + 0.5 * sin(t), 0.0, 0.0, 1.0);
+    gl_FragColor = vec4(0.5 + 0.5 * sin(t), 0.5 + 0.5 * cos(t), 0.5 + 0.5 * tan(t)  , 1.0);
 }
 
