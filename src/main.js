19cb81753d651b52d5c41f756006bfa837da19fb
Aleksandar Stanisic <ciandre23@gmail.com>
2026-06-12 12:33:17 +0200

    scene add elements

--- src/main.js
+++ src/main.js
@@ -1,5 +1,46 @@
 import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import vertexShader from "./shaders/vertex.glsl";
-import fragmentShader from "./shaders/fragment.glsl";
+import vertexShader from "./shaders/vert01.glsl";
+import fragmentShader from "./shaders/frag01.glsl";
+
+const scene = new THREE.Scene();
+let ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
+camera.position.set(0, 5, 10);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector(".webgl"),
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const plane = new THREE.PlaneGeometry(5, 5, 10, 10);
+const planeMaterial = new THREE.MeshBasicMaterial({
+  color: "#00ff26",
+});
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(5, 5, 5);
+scene.add(directionalLight);
+
+const planeMesh = new THREE.Mesh(plane, planeMaterial);
+planeMesh.rotation.x = -Math.PI / 2;
+scene.add(planeMesh);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  controls.enableDamping = true;
+
+  renderer.render(scene, camera);
+}
+animate();
