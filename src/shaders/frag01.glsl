6951c66445fa8845dbe7de47b9a7b027dadb8bc7
Aleksandar Stanisic <ciandre23@gmail.com>
2026-06-13 18:53:34 +0200

    variant

--- src/shaders/frag01.glsl
+++ src/shaders/frag01.glsl
@@ -3,7 +3,14 @@ varying vec2 vUv;
 void main()
 {
      //float str=vUv.x;
-    float str=1.0-vUv.y;
+    //float str=1.0-vUv.y;
+    //float str=vUv.y*10.9;
+   // float str=vUv.y*10.0;
+
+
+   
+    float str=mod(vUv.x*10.0,1.0);//cool
+
 
     gl_FragColor = vec4(str, str,str, 1.0);   
 }
